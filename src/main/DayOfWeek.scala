b263ffbdc5fc3dcc66d60f49cb15d9084e778a69
Konrad <konrad.poczatek@stanusch.com>
2018-01-10 18:58:25 +0100

    Creation of weeks

---
+++ src/main/DayOfWeek.scala
@@ -0,0 +1,9 @@
+object DayOfWeek {
+  def main(args: Array[String]) {
+    val lista = List("Poniedziałek", "Wtorek", "Sroda", "Czwartek", "Piątek", "Sobota", "Niedziela")
+//    println("\n 1 a")
+//    for (i <- lista) {
+//      println(i)
+//    }
+  }
+}
